d12e82c00cc86e7f30c334334ef2a7ea97c03a39
Daniela Gameiro <danielacgameiro@hotmail.com>
2023-05-17 16:49:04 +0100

    Add SamePlayer project: GetHashCode() and Equals()

---
+++ SamePlayer/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace SamePlayer
+{
+    class Program
+    {
+        static void Main()
+        {
+            ISet<Player> setOfPlayers = new HashSet<Player>();
+
+            setOfPlayers.Add(new Player() { 
+                Name = "Ana", Type = PlayerType.Tank });
+            setOfPlayers.Add(new Player() { 
+                Name = "Paulo", Type = PlayerType.Slayer });
+            setOfPlayers.Add(new Player() { 
+                Name = "Ana", Type = PlayerType.Tank });
+
+            foreach(Player p in setOfPlayers)
+                Console.WriteLine($"{p.Name} is a {p.Type}.");
+        }
+    }
+}
